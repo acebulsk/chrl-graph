2d22b5ce4a3584c06f26267d743378a5358ee1b6
Alex Cebulski <alexcebulski@gmail.com>
2024-02-10 04:14:56 +0000

    changed monthly line graph to similar to water survey monthly plots and added some error checks

--- classes/monthly_normals.r
+++ classes/monthly_normals.r
@@ -11,23 +11,40 @@ output$header4 <- renderUI({
 finalData <- reactive({
   req(input$monthly_site)
   file_path <- paste0('data/monthly_normals_plot_data/', input$monthly_site, '_monthly_normals_data.rds')
-  df <- readRDS(file_path) |> 
+  
+  validate(
+    need(file.exists(file_path),
+         'No data: Please select another station, data is not available for this station yet.')
+    )
+  
+  monthly_stats_df <- readRDS(file_path) |> 
     filter(name == input$monthly_var)
-
-  return(df)
+  
+  validate(
+  need(monthly_stats_df$name %in% input$monthly_var,
+       "No data: Please select another variable, this variable is not available for this station yet.")
+  )
+  
+  return(monthly_stats_df)
 })
 
-# globAverage <- reactive({
-#   req(input$monthly_site)
-#   file_path <- paste0('data/glob_average/', input$monthly_site, '_glob_average.rds')
-#   df <- readRDS(file_path)
-#   
-#   return(df)
-# })
-
-# output$table <- renderTable({
-#   finalData()
-# })
+yearlyData <- reactive({
+  req(input$monthly_site)
+  file_path <- paste0('data/yearly_mean_monthly_summary/', input$monthly_site, '_yearly_mean_monthly_summary.rds')
+  validate(
+    need(file.exists(file_path),
+         'No data: Please select another station, data is not available for this station yet.')
+  )
+  yearly_stats_df <- readRDS(file_path) |> 
+    filter(name == input$monthly_var)
+  
+  validate(
+    need(yearly_stats_df$name %in% input$monthly_var,
+         "No data: Please select another variable, this variable is not available for this station yet.")
+  )
+  
+  return(yearly_stats_df)
+})
 
 observe({
   req(input$monthly_site)
@@ -44,56 +61,51 @@ output$plot <- renderPlot({
   req(input$monthly_site)
   req(input$plot_type)
   req(input$monthly_year)
+
+  validate(
+    need(nrow(finalData()) > 0, 'No Data: No data exists for this variable at this station.')
+  )
   
   select_year <- input$monthly_year
-  monthly_summary <- finalData()
-  
-  monthly_summary$month_name <- month.abb[as.numeric(monthly_summary$month_num)]
-  monthly_summary$month_name <- ordered(monthly_summary$month_name, levels = c(month.abb[10:12], month.abb[1:9]))
-  
-  monthly_summary_select_year <- monthly_summary |> 
-    filter(wtr_year == select_year)
-  
-  min_year <- min(monthly_summary$wtr_year) |> as.numeric()
-  max_year <- max(monthly_summary$wtr_year) |> as.numeric()
-  year_range <- seq.int(min_year, max_year, by = 1)
-  
+  min_year <- min(finalData()$wtr_year) |> as.numeric()
+  max_year <- max(finalData()$wtr_year) |> as.numeric()
+
   y_lab <- names(monthlyVarsDict)[monthlyVarsDict == input$monthly_var]
   
-  color_palette <- c(viridisLite::viridis(n = (max_year-min_year)+1,
-                                          option = 'D'))
-  
-  col_index <- which(year_range == select_year)
-  
-  color_palette[col_index] <- 'red'
-  
-  # glob_avg <- globAverage()
-  
-  #browser()
   if(input$plot_type == 'Line Graph'){
-    gg_out <- ggplot(monthly_summary, aes(month_name, mean_monthly))  +
-      geom_line(aes(
-        colour = as.factor(wtr_year),
-        group = as.factor(wtr_year),
-      ),
-      linetype = ifelse(monthly_summary$wtr_year == select_year, 'dashed', 'solid')
-      ) +
-      geom_point(aes(colour = as.factor(wtr_year)), size = 0.5)+
-      geom_errorbar(data = monthly_summary_select_year, aes(x = month_name, ymin = min_monthly, ymax = max_monthly, colour = as.factor(wtr_year)), width = 0.25)+
-      scale_color_manual(name = 'Water Year', values = color_palette) +  # Assign colors based on the year
+    col_names <- c('line_group', 'month_name', 'value')
+    
+    monthly_summary <- finalData()  |> 
+      select(line_group = wtr_year, month_name, value = mean_monthly) |> 
+      filter(line_group %in% select_year)
+    
+    yearly_summary <- yearlyData() |> 
+      pivot_longer(c(Max:Min), names_to = 'line_group') |> 
+      select(line_group, month_name, value)
+    
+    month_line_df <- rbind(monthly_summary, yearly_summary) |> 
+      mutate(line_group = ordered(line_group, levels = c('Min', select_year, 'Max')))
+    
+    gg_out <- ggplot(month_line_df, aes(month_name, value, colour = line_group, group = line_group))  +
+      geom_line() +
+      geom_point(size = 0.5)+
+      scale_color_viridis_d(name = '') +  # Assign colors based on the year
       labs(x = "Month", y = y_lab,
-           caption = paste0('Only includes months with > 90% of data. Red error bars show the monthly min and max for the selected water year.')) +
+           caption = paste0('Mean monthly values for water years: ', min_year, ' to ', max_year-1, '.\n Only includes months with > 90% of data.')) +
       theme_bw(base_size = 14) +
       theme(legend.position = 'bottom')
       
-  } else {
-    gg_out <- ggplot(monthly_summary, aes(month_name, mean_monthly)) + 
+  } else if(input$plot_type == 'Boxplot'){
+    
+    box_data <- finalData()
+      
+    gg_out <- ggplot(box_data, aes(month_name, mean_monthly)) + 
       geom_boxplot() +
-      geom_point(data = subset(monthly_summary, wtr_year == select_year), 
+      geom_point(data = subset(box_data, wtr_year == select_year), 
                  aes(shape = "Select Water Year"), color = 'red', size = 3) +
       scale_shape_manual(values = c("Select Water Year" = 2)) +
       labs(x = "Month", y = y_lab,
-           caption = paste0('Period of Record: ', min_year, ' to ', max_year-1, '. Does not include the latest year. \nOnly includes months with > 90% of data.'))  +
+           caption = paste0('Period of Record: ', min_year, ' to ', max_year-1, '. \nOnly includes months with > 90% of data.'))  +
       theme_bw(base_size = 14) +
       theme(legend.title = element_blank())
   }
